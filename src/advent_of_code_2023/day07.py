b32eb0a2c8553bbbced663c4634acd3c8ffe6555
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-12-12 19:00:15 -0500

    Day 07 all parts.

--- src/advent_of_code_2023/day07.py
+++ src/advent_of_code_2023/day07.py
@@ -3,8 +3,6 @@ from enum import IntEnum
 
 from utils import read_lines
 
-LABELS = "23456789TJQKA"
-
 
 class HandType(IntEnum):
     HIGH_CARD = 0
@@ -16,47 +14,80 @@ class HandType(IntEnum):
     FIVE_OF_A_KIND = 6
 
 
-def get_hand_type(labels: str):
+def get_hand_type(labels: str, *, joker_active: bool) -> HandType:
     counter = Counter(labels)
     counts = counter.values()
+
+    if not joker_active or counter["J"] == 0:
+        if 5 in counts:
+            return HandType.FIVE_OF_A_KIND
+        if 4 in counts:
+            return HandType.FOUR_OF_A_KIND
+        if 3 in counts and 2 in counts:
+            return HandType.FULL_HOUSE
+        if 3 in counts and 1 in counts:
+            return HandType.THREE_OF_A_KIND
+        if 2 in counts and len(counts) == 3:
+            return HandType.TWO_PAIR
+        if 2 in counts and len(counts) == 4:
+            return HandType.ONE_PAIR
+        return HandType.HIGH_CARD
+
+    # joker active and at least one J present
     if 5 in counts:
+        # JJJJJ
         return HandType.FIVE_OF_A_KIND
     if 4 in counts:
-        return HandType.FOUR_OF_A_KIND
+        # JJJJX or XXXXJ
+        return HandType.FIVE_OF_A_KIND
     if 3 in counts and 2 in counts:
-        return HandType.FULL_HOUSE
+        # JJJXX or XXJJJ
+        return HandType.FIVE_OF_A_KIND
     if 3 in counts and 1 in counts:
-        return HandType.THREE_OF_A_KIND
+        # JJJXY or XXXYJ
+        return HandType.FOUR_OF_A_KIND
     if 2 in counts and len(counts) == 3:
-        return HandType.TWO_PAIR
+        if counter["J"] == 2:
+            # JJXXY
+            return HandType.FOUR_OF_A_KIND
+        if counter["J"] == 1:
+            # XXYYJ
+            return HandType.FULL_HOUSE
     if 2 in counts and len(counts) == 4:
-        return HandType.ONE_PAIR
-    return HandType.HIGH_CARD
+        # JJXYZ or XXYZJ
+        return HandType.THREE_OF_A_KIND
+    # WXYZJ
+    return HandType.ONE_PAIR
 
 
 class Hand:
-    def __init__(self, labels: str, bid: int):
+    def __init__(self, labels: str, bid: int, *, joker_active: bool = False):
         self.labels = labels
         self.bid = bid
-        self.hand_type = get_hand_type(labels)
+        if joker_active:
+            self.label_order = "J23456789TQKA"
+        else:
+            self.label_order = "23456789TJQKA"
+        self.hand_type = get_hand_type(labels, joker_active=joker_active)
 
     def __lt__(self, other: "Hand") -> bool:
         if self.hand_type < other.hand_type:
             return True
         if self.hand_type == other.hand_type:
             for label, other_label in zip(self.labels, other.labels):
-                if LABELS.index(label) < LABELS.index(other_label):
+                if self.label_order.index(label) < self.label_order.index(other_label):
                     return True
-                if LABELS.index(label) > LABELS.index(other_label):
+                if self.label_order.index(label) > self.label_order.index(other_label):
                     return False
         return False
 
     def __repr__(self):
-        return f"Hand({self.labels}, {self.bid})"
+        return f"Hand({self.labels}, {self.hand_type}, {self.bid})"
 
 
-# part 1
+# For part 1, need to set joker_active to False.
+# For part 2, need to set joker_active to True.
 lines = read_lines("tests/day07.input")
-hands = [Hand(labels, int(bid_str)) for labels, bid_str in [line.split() for line in lines]]
+hands = [Hand(labels, int(bid_str), joker_active=True) for labels, bid_str in [line.split() for line in lines]]
 hands.sort()
 print(sum(hand.bid * rank for rank, hand in enumerate(hands, start=1)))  # noqa: T201
